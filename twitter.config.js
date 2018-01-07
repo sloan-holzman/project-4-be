5974c8537592e8fb890b90cf40d9f942f2f9b3fc
Sloan Holzman <sloan.holzman@gmail.com>
2018-01-07 13:37:18 -0500

    set up twitter auth

---
+++ twitter.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  consumerKey: '8ybTvSiy04sXyAB1ACsAXBG9e',
+  consumerSecret: 'PLFZLbDjIIZ8qs70J6DYKjP15xfTSWAVl0HgEtsN85bfN1vhet',
+  oauth_callback: 'http://localhost/3000/twitter/callback'
+};
