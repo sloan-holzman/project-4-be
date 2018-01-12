8220fb808a9001ae5e8e8376fc867653283c3963
Sloan Holzman <sloan.holzman@gmail.com>
2018-01-12 13:40:51 -0500

    update twitter config

--- twitter.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumerKey: '8ybTvSiy04sXyAB1ACsAXBG9e',
-  consumerSecret: 'PLFZLbDjIIZ8qs70J6DYKjP15xfTSWAVl0HgEtsN85bfN1vhet',
-  oauth_callback: 'http://virtual-wallet.surge.sh/',
-  secret: 'w4879oiyufgcgu8td'
-};
