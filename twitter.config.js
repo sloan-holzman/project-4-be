e30b49724902524f67f6fd2b306509c8542710e8
Sloan Holzman <sloan.holzman@gmail.com>
2018-01-18 18:04:05 -0500

    reverted state to last deployment that worked

---
+++ twitter.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumerKey: '4aHWwqvBQ2iAFBiX5f5y0SBkS',
+  consumerSecret: '5CKhWwTYSIpkgfw4fXyi7ia72HN10IWSeWTfqRWIjF596XZXzl',
+  oauth_callback: 'http://127.0.0.1:3000',
+  secret: 'w4879oiyufgcgu8td'
+};
