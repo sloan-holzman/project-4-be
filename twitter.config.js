b5c6b940533b2bab0c35d4d9e4884bc5ee3fdca7
Sloan Holzman <sloan.holzman@gmail.com>
2018-01-18 18:05:05 -0500

    change callback to virtual-wallet.surge.sh

--- twitter.config.js
+++ twitter.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   consumerKey: '4aHWwqvBQ2iAFBiX5f5y0SBkS',
   consumerSecret: '5CKhWwTYSIpkgfw4fXyi7ia72HN10IWSeWTfqRWIjF596XZXzl',
-  oauth_callback: 'http://127.0.0.1:3000',
+  oauth_callback: 'http://virtual-wallet.surge.sh',
   secret: 'w4879oiyufgcgu8td'
 };
