af96d8dc17d25674c33c01897ceec649fd9dbca5
Sloan Holzman <sloan.holzman@gmail.com>
2018-01-18 09:59:08 -0500

    change back to origin *

--- routes/loginroutes.js
+++ routes/loginroutes.js
@@ -19,7 +19,7 @@ module.exports = function(app){
 
   // enable cors
   var corsOption = {
-    origin: "https://virtual-wallet.surge.sh",
+    origin: "*",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     exposedHeaders: ['x-auth-token']
